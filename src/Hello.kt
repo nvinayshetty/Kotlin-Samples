05aa36853fe4a97115e3cdf0c7e5bfc8a1a83eaa
vinaya.n@redbus.com <vinaya.n@redbus.com>
2017-04-10 23:18:16 +0530

    Type declaration

--- src/Hello.kt
+++ src/Hello.kt
@@ -2,5 +2,23 @@
  * Created by vinayaprasadn on 10/4/17.
  */
 fun main(args:Array<String>){
-    println("Hello world Kotlin!")
+    declareVariables()
 }
+
+fun declareVariables() {
+    val a:Int=5
+    val b:Float=2.3f
+    val c:Double=2.35e2
+    val d:Long=500
+    val f:String="Hello"
+    println("$a $b $c $d $f")
+
+    val e:Int //Need not to initialize the value at the time of declaration
+    e=4 //val is finally once the first value os assigned
+    //e=5 you
+
+    var g:Int=23
+    g=45
+
+}
+
