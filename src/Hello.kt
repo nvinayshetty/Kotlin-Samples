dc56996336dccae50a208f0b18f2eb54cc769d87
vinaya.n@redbus.com <vinaya.n@redbus.com>
2017-04-11 08:22:06 +0530

    Int range

--- src/Hello.kt
+++ src/Hello.kt
@@ -2,7 +2,19 @@
  * Created by vinayaprasadn on 10/4/17.
  */
 fun main(args:Array<String>){
-    declareVariables()
+    //declareVariables()
+    ranges()
+}
+
+fun ranges() {
+   //val a:IntRange=1..10
+    //val  a=1.rangeTo(10)
+  /*  val  a: IntProgression =1.rangeTo(10).reversed()*/
+    val a:IntProgression=100 downTo 0 step 3
+   for(x in a ) println("$x\t")
+    println("sum of ints $a=${a.sum()}")
+
+
 }
 
 fun declareVariables() {
