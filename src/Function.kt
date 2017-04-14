3e0a3144f44ec6e8f9d0c50e97355f7b2a7738c2
vinaya.n@redbus.com <vinaya.n@redbus.com>
2017-04-14 12:33:01 +0530

    Functions

---
+++ src/Function.kt
@@ -0,0 +1,13 @@
+/**
+ * Created by vinayaprasadn on 14/4/17.
+ */
+package kotlinsamples.functions
+fun sayHello(){
+    println("Hello")
+}
+
+    /*fun triple(x:Int):Int{
+        return 3*x
+    }*/
+
+fun triple(x:Int)=3*x
---
+++ src/HighlevelFunction.kt
@@ -0,0 +1,8 @@
+/**
+ * Created by vinayaprasadn on 14/4/17.
+ */
+package kotlinsamples.functions
+
+fun main(args:Array<String>){
+ println("Triple "+triple(4))
+}
